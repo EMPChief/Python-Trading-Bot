8fd311f41ad13f7ccb0a87ff369709e26262cc66
ITapan <70442425+ITapan@users.noreply.github.com>
2023-11-19 20:20:08 +0100

    fixed variables

--- fun.py
+++ fun.py
@@ -10,21 +10,21 @@ cart = []
 total = 0
 
 print("------Menu------")
-for key, value in buyingdict.items():  # Fix typo in variable name
-    print(f"{key:15}: ${value:.2f}")  # Fix typo and format value
+for key, value in buyingdict.items():
+    print(f"{key:15}: ${value:.2f}")
 print("----------------")
 
 while True:
     buying = input("Enter what you want to buy: (q to quit)").lower()
     if buying == "q" or buying == "quit":
         break
-    elif buying in buyingdict:  # Simplify condition
-        cart.append(buying)  # Append the item, not the entire dictionary
+    elif buying in buyingdict:
+        cart.append(buying)
 
 print(cart)
 print("-------your order-------")
 for buy in cart:
-    total += buyingdict.get(buy)  # Fix variable name
+    total += buyingdict.get(buy)
     print(f"{buy:15}: ${buyingdict.get(buy):.2f}")
 print(f"Total: ${total:.2f}")
 print("-----------------------")
