16e62e83e72ee0e4d94b6951b6043a6c7ace7f78
ITapan <70442425+ITapan@users.noreply.github.com>
2023-11-27 10:25:29 +0100

    moved some stuff around

--- models/instruments.py
+++ models/instruments.py
@@ -5,30 +5,30 @@ class Instrument:
     Attributes:
         name (str): The name of the instrument.
         ins_type (str): The type of the instrument.
-        display_name (str): The display name of the instrument.
-        pip_location (int): The location of the pip (power of 10).
-        trade_units_precision (int): The precision of trade units.
-        margin_rate (float): The margin rate for the instrument.
+        displayName (str): The display name of the instrument.
+        pipLocation (int): The location of the pip (power of 10).
+        tradeUnitsPrecision (int): The precision of trade units.
+        marginRate (float): The margin rate for the instrument.
     """
 
-    def __init__(self, name, ins_type, display_name, pip_location, trade_units_precision, margin_rate):
+    def __init__(self, name, ins_type, displayName, pipLocation, tradeUnitsPrecision, marginRate):
         """
         Initializes an Instrument object.
 
         Args:
             name (str): The name of the instrument.
             ins_type (str): The type of the instrument.
-            display_name (str): The display name of the instrument.
-            pip_location (int): The location of the pip (power of 10).
-            trade_units_precision (int): The precision of trade units.
-            margin_rate (float): The margin rate for the instrument.
+            displayName (str): The display name of the instrument.
+            pipLocation (int): The location of the pip (power of 10).
+            tradeUnitsPrecision (int): The precision of trade units.
+            marginRate (float): The margin rate for the instrument.
         """
         self.name = name
         self.ins_type = ins_type
-        self.display_name = display_name
-        self.pip_location = pow(10, pip_location)
-        self.trade_units_precision = trade_units_precision
-        self.margin_rate = float(margin_rate)
+        self.displayName = displayName
+        self.pipLocation = pow(10, pipLocation)
+        self.tradeUnitsPrecision = tradeUnitsPrecision
+        self.marginRate = float(marginRate)
 
     def __repr__(self):
         """
@@ -40,21 +40,21 @@ class Instrument:
         return str(vars(self))
 
     @classmethod
-    def FromApiObject(cls, api_object):
+    def FromApiObject(cls, ob):
         """
         Creates an Instrument object from an API response object.
 
         Args:
-            api_object (dict): The API response object.
+            ob (dict): The API response object.
 
         Returns:
             Instrument: An Instrument object created from the API response.
         """
         return Instrument(
-            api_object.get('name', ''),
-            api_object.get('ins_type', ''),
-            api_object.get('displayName', ''),
-            api_object.get('pipLocation', ''),
-            api_object.get('tradeUnitsPrecision', ''),
-            api_object.get('marginRate', '')
+            ob.get('name', ''),
+            ob.get('ins_type', ''),
+            ob.get('displayName', ''),
+            ob.get('pipLocation', ''),
+            ob.get('tradeUnitsPrecision', ''),
+            ob.get('marginRate', '')
         )
