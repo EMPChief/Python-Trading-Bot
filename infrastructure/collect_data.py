a81eca6f248b557b02a42d75e4988e02d2e5ced3
itapancc <bjornmagnekristensen1999@gmail.com>
2023-12-06 21:07:18 +0100

    fixed some bugs

--- infrastructure/collect_data.py
+++ infrastructure/collect_data.py
@@ -8,28 +8,50 @@ from api.oanda_api import OandaApi
 CANDLE_COUNT = 3000
 
 INCREMENTS = {
-    'M5' : 5 * CANDLE_COUNT,
+    'M5': 5 * CANDLE_COUNT,
     'M15': 15 * CANDLE_COUNT,
-    'H1' : 60 * CANDLE_COUNT,
-    'H4' : 240 * CANDLE_COUNT,
-    'D1' : 1440 * CANDLE_COUNT,
+    'H1': 60 * CANDLE_COUNT,
+    'H4': 240 * CANDLE_COUNT,
+    'D1': 1440 * CANDLE_COUNT,
 }
 
 
 def save_file(final_df: pd.DataFrame, file_prefix, granularity, pair):
+    """
+    Saves a pandas DataFrame containing candle data to a CSV file with a specified filename.
+
+    Parameters:
+        - final_df (pd.DataFrame): The DataFrame containing candle data.
+        - file_prefix (str): The prefix for the CSV file name.
+        - granularity (str): The granularity of the candle data.
+        - pair (str): The currency pair for which the data is collected.
+    """
     filename = f"{file_prefix}{pair}_{granularity}.csv"
 
     final_df.drop_duplicates(subset=['time'], inplace=True)
     final_df.sort_values(by='time', inplace=True)
     final_df.reset_index(drop=True, inplace=True)
-    final_df.to_csv(filename);
+    final_df.to_csv(filename)
 
-    s1 = f"*** {pair} {granularity} {final_df.time.min()} {final_df.time.max()}"
+    s1 = f"*** {pair} {granularity} {final_df.time.min()
+                                     } {final_df.time.max()}"
     print(f"*** {s1} --> {final_df.shape[0]} candles ***")
 
 
-def fetch_candles(pair, granularity, date_f: dt.datetime, 
-                    date_t: dt.datetime, api: OandaApi ):
+def fetch_candles(pair, granularity, date_f: dt.datetime, date_t: dt.datetime, api: OandaApi):
+    """
+    Fetches candle data for a given currency pair and granularity within a specified date range.
+
+    Parameters:
+        - pair (str): The currency pair for which to fetch data.
+        - granularity (str): The granularity of the candle data.
+        - date_f (dt.datetime): The start date for the data retrieval.
+        - date_t (dt.datetime): The end date for the data retrieval.
+        - api (OandaApi): An instance of the OandaApi class.
+
+    Returns:
+        - pd.DataFrame or None: The fetched candle data if successful, otherwise None.
+    """
 
     attempts = 0
 
@@ -53,8 +75,19 @@ def fetch_candles(pair, granularity, date_f: dt.datetime,
         return None
 
 
-def collect_data(pair, granularity, date_f, date_t, file_prefix, api: OandaApi ):
-    
+def collect_data(pair, granularity, date_f, date_t, file_prefix, api: OandaApi):
+    """
+    Collects candle data iteratively for a specified currency pair and granularity, saving it to CSV files.
+
+    Parameters:
+        - pair (str): The currency pair for which to collect data.
+        - granularity (str): The granularity of the candle data.
+        - date_f (str): The start date for the data collection in string format.
+        - date_t (str): The end date for the data collection in string format.
+        - file_prefix (str): The prefix for the CSV file names.
+        - api (OandaApi): An instance of the OandaApi class.
+    """
+
     time_step = INCREMENTS[granularity]
 
     end_date = parser.parse(date_t)
@@ -71,7 +104,7 @@ def collect_data(pair, granularity, date_f, date_t, file_prefix, api: OandaApi )
 
         candles = fetch_candles(
             pair,
-            granularity, 
+            granularity,
             from_date,
             to_date,
             api
@@ -79,12 +112,13 @@ def collect_data(pair, granularity, date_f, date_t, file_prefix, api: OandaApi )
 
         if candles is not None:
             candle_dfs.append(candles)
-            print(f"{pair} {granularity} {from_date} {to_date} --> {candles.shape[0]} candles loaded")
+            print(f"{pair} {granularity} {from_date} {
+                  to_date} --> {candles.shape[0]} candles loaded")
         else:
             print(f"{pair} {granularity} {from_date} {to_date} --> NO CANDLES")
-        
+
         from_date = to_date
-    
+
     if len(candle_dfs) > 0:
         final_df = pd.concat(candle_dfs)
         save_file(final_df, file_prefix, granularity, pair)
@@ -92,8 +126,14 @@ def collect_data(pair, granularity, date_f, date_t, file_prefix, api: OandaApi )
         print(f"{pair} {granularity} --> NO DATA SAVED!")
 
 
-
 def run_collection(ic: InstrumentCollection, api: OandaApi):
+    """
+    Runs the data collection for specified currency pairs and granularities.
+
+    Parameters:
+        - ic (InstrumentCollection): An instance of the InstrumentCollection class.
+        - api (OandaApi): An instance of the OandaApi class.
+    """
     our_curr = ["AUD", "CAD", "JPY", "USD", "EUR", "GBP", "NZD"]
     for p1 in our_curr:
         for p2 in our_curr:
@@ -104,33 +144,8 @@ def run_collection(ic: InstrumentCollection, api: OandaApi):
                     collect_data(
                         pair,
                         granularity,
-                        "2016-01-07T00:00:00Z",
-                        "2021-12-31T00:00:00Z",
+                        "2017-01-07T00:00:00Z",
+                        "2023-11-30T00:00:00Z",
                         "./data/candles/",
                         api
                     )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- main_get_tradinginfo.py
+++ main_get_tradinginfo.py
@@ -5,7 +5,8 @@ from dateutil import parser
 from infrastructure.collect_data import run_collection
 
 if __name__ == '__main__':
-    api = OandaApi()
-    instrumentCollection.LoadInstruments("./data")
-    run_collection(instrumentCollection, api)
-    # run_ma_sim()
+    # api = OandaApi()
+    # instrumentCollection.LoadInstruments("./data")
+    # run_collection(instrumentCollection, api)
+    #run_ma_sim()
+
