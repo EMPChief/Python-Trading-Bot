84c9199d814409fbe3b571cc0f0ce27e1d8460dc
ITapan <70442425+ITapan@users.noreply.github.com>
2023-12-06 21:26:12 +0100

    added plotly to req

--- main_get_tradinginfo.py
+++ main_get_tradinginfo.py
@@ -1,12 +1,11 @@
 from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
 from simulation.ma_cross import run_ma_sim
-from dateutil import parser
-from infrastructure.collect_data import run_collection
-
 if __name__ == '__main__':
-    # api = OandaApi()
-    # instrumentCollection.LoadInstruments("./data")
-    # run_collection(instrumentCollection, api)
-    #run_ma_sim()
-
+    api = OandaApi()
+    
+    instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
+    instrumentCollection.LoadInstruments("./data")
+    #instrumentCollection.PrintInstruments()
+    #run_ma_sim(curr_list=["EUR", "USD", "GBP", "JPY", "AUD", "CAD"])
+    
