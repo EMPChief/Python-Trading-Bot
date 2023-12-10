a78df7ebce5515c18728089306d28a9eec9489ac
empchief <bjornmagnekristensen1999@gmail.com>
2023-12-10 15:53:18 +0100

    fixed styling

--- exploration/plotting.py
+++ exploration/plotting.py
@@ -1,64 +1,23 @@
 import pandas as pd
 import plotly.graph_objects as go
-import datetime as dt
 from plotly.subplots import make_subplots
 
 
 class CandlePlot:
-    """
-    A class for creating candlestick charts using Plotly.
-
-    Parameters:
-    - df: pandas DataFrame
-        Input DataFrame containing candlestick data.
-    - candles: bool, optional
-        Flag to include candlestick chart (default is True).
-
-    Methods:
-    - add_timestring(): Adds a formatted time string column to the DataFrame.
-    - create_candle_fig(): Creates a candlestick chart using Plotly.
-    - update_layout(width, height, nticks): Updates the layout of the chart.
-    - add_traces(line_traces): Adds line traces to the chart.
-    - show_plot(width=1500, height=800, nticks=5, line_traces=[]): Displays the candlestick chart with optional line traces.
-
-    Attributes:
-    - df_plot: pandas DataFrame
-        Copy of the input DataFrame for chart plotting.
-
-    Example:
-    cp = CandlePlot(df, candles=True)
-    cp.show_plot()
-    """
-
-    def __init__(self, df, candles=True):
-        """
-        Initializes the CandlePlot object.
-
-        Parameters:
-        - df: pandas DataFrame
-            Input DataFrame containing candlestick data.
-        - candles: bool, optional
-            Flag to include candlestick chart (default is True).
-        """
+    def __init__(self, df: pd.DataFrame, candles: bool = True):
         self.df_plot = df.copy()
         self.candles = candles
         self.create_candle_fig()
 
     def add_timestring(self):
-        """
-        Adds a formatted time string column to the DataFrame.
-        """
         self.df_plot['time'] = pd.to_datetime(self.df_plot['time'])
-        self.df_plot['sTime'] = [dt.datetime.strftime(
-            x, "s%Y-%m-%d %H:%M") for x in self.df_plot['time']]
+        self.df_plot['sTime'] = [x.strftime(
+            "s%Y-%m-%d %H:%M") for x in self.df_plot['time']]
 
     def create_candle_fig(self):
-        """
-        Creates a candlestick chart using Plotly.
-        """
         self.add_timestring()
         self.fig = make_subplots(specs=[[{"secondary_y": True}]])
-        if self.candles == True:
+        if self.candles:
             self.fig.add_trace(go.Candlestick(
                 x=self.df_plot.sTime,
                 open=self.df_plot.mid_o,
@@ -69,48 +28,31 @@ class CandlePlot:
                 opacity=1,
                 increasing=dict(fillcolor='#00cc00', line_color='#006600'),
                 decreasing=dict(fillcolor='#ff0000', line_color='#990000'),
+                hovertext='Candlestick',
+                name='Candlestick'
             ))
 
-    def update_layout(self, width, height, nticks):
-        """
-        Updates the layout of the chart.
-
-        Parameters:
-        - width: int
-            Width of the chart.
-        - height: int
-            Height of the chart.
-        - nticks: int
-            Number of ticks on the x-axis.
-        """
-        self.fig.update_yaxes(gridcolor='gray')
-        self.fig.update_xaxes(
-            gridcolor='gray',
-            rangeslider=dict(visible=False),
-            nticks=nticks)
-
+    def update_layout(self, width: int, height: int, nticks: int):
+        self.fig.update_yaxes(gridcolor='lightgray', title_text='Price')
+        self.fig.update_xaxes(gridcolor='lightgray', rangeslider=dict(
+            visible=False), nticks=nticks, title_text='Time')
         self.fig.update_layout(
             width=width,
             height=height,
-            title='Bjørn Chart',
-            xaxis_title='Time',
-            yaxis_title='Mid Prices',
+            title='Trading Chart',
             margin=dict(l=10, r=10, b=10, t=10),
             paper_bgcolor='black',
             plot_bgcolor='black',
-            font=dict(color='white', size=12)
+            font=dict(color='white', size=12),
+            legend=dict(
+                yanchor="top",
+                y=0.99,
+                xanchor="left",
+                x=0.01
+            )
         )
 
-    def add_traces(self, line_traces, is_secondary=False):
-        """
-        Adds line traces to the chart.
-
-        Parameters:
-        - line_traces: list
-            List of column names to be used as line traces on the chart.
-        - is_secondary: bool, optional
-            Flag indicating whether the trace should be on the secondary y-axis.
-        """
+    def add_traces(self, line_traces: list, is_secondary: bool = False):
         for t in line_traces:
             self.fig.add_trace(go.Scatter(
                 x=self.df_plot.sTime,
@@ -118,24 +60,10 @@ class CandlePlot:
                 line=dict(width=2),
                 line_shape="spline",
                 name=t,
+                hovertext=t,
             ), secondary_y=is_secondary)
 
-    def show_plot(self, width=1500, height=800, nticks=5, line_traces=[], sec_traces=[]):
-        """
-        Displays the candlestick chart with optional line traces.
-
-        Parameters:
-        - width: int, optional
-            Width of the chart (default is 1500).
-        - height: int, optional
-            Height of the chart (default is 800).
-        - nticks: int, optional
-            Number of ticks on the x-axis (default is 5).
-        - line_traces: list, optional
-            List of column names to be used as line traces on the chart.
-        - sec_traces: list, optional
-            List of column names for line traces on the secondary y-axis.
-        """
+    def show_plot(self, width: int = 1500, height: int = 800, nticks: int = 5, line_traces: list = [], sec_traces: list = []):
         self.add_traces(line_traces)
         self.add_traces(sec_traces, is_secondary=True)
         self.update_layout(width, height, nticks)
