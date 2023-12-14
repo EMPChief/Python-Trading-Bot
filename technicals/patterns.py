fc2a8e52f6b1c5face8e3fe100a103a6fa7edd36
empchief <bjornmagnekristensen1999@gmail.com>
2023-12-14 18:59:28 +0100

    did some candle pattern

---
+++ technicals/patterns.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+
+def apply_candle_props(df: pd.DataFrame):
+
+    df_an = df.copy()
+    direction = df_an.mid_c - df_an.mid_o
+    body_size = abs(direction)
+    direction = [1 if x >= 0 else -1 for x in direction]
+    full_range = df_an.mid_h - df_an.mid_l
+    body_percentage = (body_size / full_range) * 100
+    body_lower = df_an[['mid_c', 'mid_o']].min(axis=1)
+    body_upper = df_an[['mid_c', 'mid_o']].max(axis=1)
+    body_bottom_percentage = ((body_lower - df_an.mid_l) / full_range) * 100
+    body_top_percentage = 100 - \
+        (((df_an.mid_h - body_upper) / full_range) * 100)
+
+    mid_point = full_range / 2 + df_an.mid_l
+
+    low_change = df_an.mid_l.pct_change() * 100
+    high_change = df_an.mid_h.pct_change() * 100
+    body_size_change = body_size.pct_change() * 100
+
+    df_an['body_lower'] = body_lower
+    df_an['body_upper'] = body_upper
+    df_an['body_bottom_percentage'] = body_bottom_percentage
+    df_an['body_top_percentage'] = body_top_percentage
+    df_an['body_percentage'] = body_percentage
+    df_an['direction'] = direction
+    df_an['body_size'] = body_size
+    df_an['low_change'] = low_change
+    df_an['high_change'] = high_change
+    df_an['body_size_change'] = body_size_change
+    df_an['mid_point'] = mid_point
+    df_an['mid_point_prev_2'] = mid_point.shift(2)
+    df_an['body_size_prev'] = df_an.body_size.shift(1)
+    df_an['direction_prev'] = df_an.direction.shift(1)
+    df_an['direction_prev_2'] = df_an.direction.shift(2)
+    df_an['body_percentage_prev'] = df_an.body_percentage.shift(1)
+    df_an['body_percentage_prev_2'] = df_an.body_percentage.shift(2)
+
+    return df_an
+
+
+def apply_patterns(df: pd.DataFrame):
+    df_an = apply_candle_props(df)
+    return df_an
