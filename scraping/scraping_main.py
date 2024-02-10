4187430ccee5f68814a5747f18e67a9919eee956
EMPChief <bjornmagnekristensen1999@gmail.com>
2024-02-10 16:22:54 +0100

    Refactor scraping_main.py and update scraping interval to make it more readable

--- scraping/scraping_main.py
+++ scraping/scraping_main.py
@@ -2,31 +2,47 @@ from dailyfx_com import DailyFXScraper
 from investing_com import InvestingComScraper
 import time
 import json
-scraping_interval = 3600
+
+
+scraping_interval = 180
 data_path = "data/scraping"
+
 if __name__ == "__main__":
-#    scraper = DailyFXScraper()
+    scraper = DailyFXScraper()
+    investing_scraper = InvestingComScraper()
     while True:
         print("Scraping data...")
-#        print("Scraping DailyFX data...")
-#        sentiment_dataframe = scraper.extract_sentiment_data()
-#        print(sentiment_dataframe)
-#        sentiment_dataframe.to_csv("data/scraping/dailyfx_sentiment.csv", mode='a', index=False, header=False)
-#        time.sleep(scraping_interval)
-#    scraper = DailyFXScraper()
-#    while True:
-#        sentiment_dataframe = scraper.extract_sentiment_data()
-#        data = sentiment_dataframe.to_dict(orient='records')
-#        with open("data/scraping/dailyfx_sentiment.json", "a") as f:
-#            json.dump(data, f, indent=4)
-#            f.write("\n")
+        
+        # Scraping DailyFX data
+        print("Scraping DailyFX data...")
+        sentiment_dataframe = scraper.extract_sentiment_data()
+        print(sentiment_dataframe)
+        
+        # Save DailyFX data to CSV
+        sentiment_dataframe.to_csv(
+            f"{data_path}/dailyfx_sentiment.csv", mode='a', index=False, header=True)
+
+        # Save DailyFX data to JSON
+        data = sentiment_dataframe.to_dict(orient='records')
+        with open(f"{data_path}/dailyfx_sentiment.json", "a") as f:
+            json.dump(data, f, indent=4)
+            f.write("\n")
+
+        # Scraping Investor.com data
         print("Scraping Investor data...")
-        investing_scraper = InvestingComScraper()
         investor_response = investing_scraper.scrape_all_data()
-        investor_response.to_csv(f"{data_path}/investor_data.csv", mode='a', index=False, header=False)
+        
+        # Save Investor.com data to CSV
+        investor_response.to_csv(
+            f"{data_path}/investor_data.csv", mode='a', index=False, header=True)
+
+        # Save Investor.com data to JSON
         data = investor_response.to_dict(orient='records')
         with open(f"{data_path}/investor_data.json", "a") as f:
             json.dump(data, f, indent=4)
             f.write("\n")
+        
         print("Done scraping data.")
+        
+        # Wait for the scraping interval before the next iteration
         time.sleep(scraping_interval)
