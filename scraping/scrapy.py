0051217048e83577192400b9ad2a5e47f7971c55
EMPChief <bjornmagnekristensen1999@gmail.com>
2024-02-10 12:29:40 +0100

    Update sentiment data file names

--- scraping/scrapy.py
+++ scraping/scrapy.py
@@ -8,13 +8,13 @@ if __name__ == "__main__":
 #    while True:
 #        sentiment_dataframe = scraper.extract_sentiment_data()
 #        print(sentiment_dataframe)
-#        sentiment_dataframe.to_csv("data/scraping/sentiment.csv", mode='a', index=False, header=False)
+#        sentiment_dataframe.to_csv("data/scraping/dailyfx_sentiment.csv", mode='a', index=False, header=False)
 #        time.sleep(scraping_interval)
     scraper = DailyFXScraper()
     while True:
         sentiment_dataframe = scraper.extract_sentiment_data()
         data = sentiment_dataframe.to_dict(orient='records')
-        with open("data/scraping/sentiment.json", "a") as f:
+        with open("data/scraping/dailyfx_sentiment.json", "a") as f:
             json.dump(data, f, indent=4)
             f.write("\n")
         time.sleep(scraping_interval)
