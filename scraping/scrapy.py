2470493f337b7409f7841bb681c7cd1d3e1beea3
EMPChief <bjornmagnekristensen1999@gmail.com>
2024-02-10 12:38:50 +0100

    Commented out scraping code

--- scraping/scrapy.py
+++ scraping/scrapy.py
@@ -10,11 +10,12 @@ if __name__ == "__main__":
 #        print(sentiment_dataframe)
 #        sentiment_dataframe.to_csv("data/scraping/dailyfx_sentiment.csv", mode='a', index=False, header=False)
 #        time.sleep(scraping_interval)
-    scraper = DailyFXScraper()
-    while True:
-        sentiment_dataframe = scraper.extract_sentiment_data()
-        data = sentiment_dataframe.to_dict(orient='records')
-        with open("data/scraping/dailyfx_sentiment.json", "a") as f:
-            json.dump(data, f, indent=4)
-            f.write("\n")
-        time.sleep(scraping_interval)
+#    scraper = DailyFXScraper()
+#    while True:
+#        sentiment_dataframe = scraper.extract_sentiment_data()
+#        data = sentiment_dataframe.to_dict(orient='records')
+#        with open("data/scraping/dailyfx_sentiment.json", "a") as f:
+#            json.dump(data, f, indent=4)
+#            f.write("\n")
+#        time.sleep(scraping_interval)
+    pass
