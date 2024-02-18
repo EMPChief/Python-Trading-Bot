43bf3c2055773067e8202c81921056f6205b3d0b
EMPChief <bjornmagnekristensen1999@gmail.com>
2024-02-18 17:13:27 +0100

    Refactor logging and error handling in server.py

--- infrastructure/log_wrapper.py
+++ infrastructure/log_wrapper.py
@@ -17,7 +17,6 @@ class LogWrapper:
 
         file_handler = logging.FileHandler(self.filename, mode=mode)
         formatter = logging.Formatter(LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
-
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
 
@@ -26,3 +25,18 @@ class LogWrapper:
     def create_directory(self):
         if not os.path.exists(LogWrapper.PATH):
             os.makedirs(LogWrapper.PATH)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def exception(self, message):
+        self.logger.exception(message)
